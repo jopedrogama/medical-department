d8aaf907dce78d0d1ae1454b8181f9b15ace18d0
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2025-03-27 04:30:32 -0700

    Update 06-Create-Billing.sql

--- Relational/00 - DDL (Data Definition Language)/06-Create-Billing.sql
+++ Relational/00 - DDL (Data Definition Language)/06-Create-Billing.sql
@@ -1,6 +1,6 @@
 CREATE TABLE billings (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  appointment_id INTEGER NOT NULL,
+  appointment_id INTEGER NOT NULL UNIQUE,
   amount DECIMAL(10,2) NOT NULL,
   payment_status ENUM('Pending', 'Paid', 'Failed', 'Refunded') NOT NULL DEFAULT 'Pending',
   payment_date TIMESTAMP NULL DEFAULT NULL,
